jmptrader/go-birect

commit dd5c6f322114684355c7906235fe092c177504e4

Capture stack traces for handler panics

birect-conn-json.go
@@ -68,13 +68,26 @@ func (c *Conn) handleJSONWireReq(wireReq *wire.Request) {
 			c.sendErrorResponse(wireReq, errs.Wrap(err, errs.Info{"Name": wireReq.Name, "Data": wireReq.Data}))
 		}
 	}()
-	resVal, err := handler(&JSONReq{c, wireReq.Data})
+	resVal, err := _runJSONHandler(handler, &JSONReq{c, wireReq.Data})
 	if err != nil {
 		c.sendErrorResponse(wireReq, errs.Wrap(err, errs.Info{"HandlerName": wireReq.Name}))
 		return
 	}
 	// Send response
 	c.sendResponse(wireReq, &jsonRes{resVal})
+}
+
+func _runJSONHandler(handler JSONReqHandler, jsonReq *JSONReq) (res interface{}, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			if rErr, ok := r.(error); ok {
+				err = rErr
+			} else {
+				err = errs.New(nil, fmt.Sprint(r))
+			}
+		}
+	}()
+	return handler(jsonReq)
 }
 
 type jsonRes struct {

birect-conn-protobuf.go
@@ -56,13 +56,28 @@ func (c *Conn) handleProtoWireReq(wireReq *wire.Request) {
 		return
 	}
 	// Execute handler
-	resVal, err := handler(&ProtoReq{c, wireReq.Data})
+	resVal, err := _runProtoHandler(handler, &ProtoReq{c, wireReq.Data})
 	if err != nil {
 		c.sendErrorResponse(wireReq, err)
 		return
 	}
 	// Send response
 	c.sendResponse(wireReq, &protoRes{resVal})
+}
+
+func _runProtoHandler(handler ProtoReqHandler, protoReq *ProtoReq) (res Proto, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			if errsErr, ok := r.(errs.Err); ok {
+				err = errsErr
+			} else if stdErr, ok := r.(error); ok {
+				err = errs.Wrap(stdErr, nil)
+			} else {
+				err = errs.New(nil, fmt.Sprint(r))
+			}
+		}
+	}()
+	return handler(protoReq)
 }
 
 type protoRes struct {

commit df36b0063b393fd3e94618ead9de6cd25a52441d

Fix build error (introduced in dd5c6f322114684355c)

birect-conn-json.go
@@ -2,6 +2,7 @@ package birect
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/marcuswestin/go-birect/internal/wire"
 	"github.com/marcuswestin/go-errs"

birect-conn-protobuf.go
@@ -1,6 +1,8 @@
 package birect
 
 import (
+	"fmt"
+
 	"github.com/golang/protobuf/proto"
 	"github.com/marcuswestin/go-birect/internal/wire"
 	"github.com/marcuswestin/go-errs"